039691d30b840fed688a010fa492fe5fd94a22bd
DanielMM161 <daniel-moreno-marquez@hotmail.es>
2021-02-01 17:49:37 +0100

    creando hook use near script

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,11 @@ module.exports = {
 				use: {
 					loader: 'babel-loader',
 					options: {
-						presets: ["@babel/preset-env", "@babel/preset-react"]
+						plugins:['@babel/plugin-syntax-dynamic-import'],
+						presets: [
+							"@babel/preset-env", 
+							"@babel/preset-react"
+						]
 					}
 				}
 			}
