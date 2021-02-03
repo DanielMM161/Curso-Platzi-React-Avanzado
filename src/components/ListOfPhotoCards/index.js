f15925d363b989c012cb17c6c35eb27e234de4e0
DanielMM161 <daniel-moreno-marquez@hotmail.es>
2021-02-03 06:48:46 +0100

    Añadiendo Apollo

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { PhotoCard } from '../PhotoCard';
 
+import {gql, useQuery} from '@apollo/client'
+
+const GET_PHOTOS = gql`
+    query getPhotos {
+        photos {
+        id
+        categoryId
+        src
+        likes
+        userId
+        liked
+        }
+    }
+`
+
 export const ListOfPhotoCards = () => {
+    const { loading, error, data} = useQuery(GET_PHOTOS)
+
+    if (loading) return <h1>...Cargando</h1>
+    if (error) return <p>Error</p>
+
     return(
         <ul>
             {
-                [1, 2, 3,4,5,6].map(id => <PhotoCard key={id} id={id} />)
+                data.photos.map((photoCard, id) =>(
+                    <PhotoCard key={id} {...photoCard} />  
+                ))
             }
         </ul>
     )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
+
 import {App} from './App'
 
+const client = new ApolloClient({
+  uri: 'https://petgram-api-daniel.now.sh/graphql',
+  cache: new InMemoryCache(),
+})
 
-ReactDom.render(<App />,
+
+ReactDom.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('app'))
