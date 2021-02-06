a7e0d7f8976b4a319ae9eb9a9cdae330b94d4fac
DanielMM161 <daniel-moreno-marquez@hotmail.es>
2021-02-06 11:32:35 +0100

    NavBar Components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React,{Fragment} from 'react';
 import {GlobalStyle} from './styles/GlobalStyles';
-import {PhotoCardWithQuery} from './container/PhotoCardWithQuery'
-import {Home} from './pages/Home'
 import {Logo} from './components/Logo'
+import {NavBar} from './components/NavBar'
 import {Router} from '@reach/router'
 
+import {Home} from './pages/Home'
+import {Detail} from './pages/Detail'
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-
-  return(
+  
+return(
     <Fragment>
       <GlobalStyle/>
       <Logo />
-      {
-        detailId 
-          ? <PhotoCardWithQuery id={detailId}/>
-          : <Router>
-              <Home path='/' />
-              <Home path='/pet/:categoryId'/>
-            </Router>
-      }
+ 
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:categoryId'/>
+        <Detail path='/detail/:detailId' />
+      </Router>
+
+      <NavBar />
     </Fragment>
   )
 }
