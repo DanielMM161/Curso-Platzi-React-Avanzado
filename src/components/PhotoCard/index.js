9aa629e5ab53579b2fde7306d0c93c1b6990c5e9
DanielMM161 <daniel-moreno-marquez@hotmail.es>
2021-02-22 21:32:34 +0100

    like at photo at register user

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,5 @@
 import React, {Fragment} from 'react';
 import { ImgWrapper, Img, Article } from './styles';
-import {useLocalStorage} from '../../hooks/useLocalStorage'
 import {useNearScreen} from '../../hooks/useNearScreen'
 import {FavButton} from '../FavButton'
 import {ToggleLikeMutation} from '../../container/ToggleLikeMutation'
@@ -8,10 +7,8 @@ import {Link} from '@reach/router'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
 
-    const key = `like-${id}`
-    const [liked,setLiked] = useLocalStorage(key,false);
     const [show, ref] = useNearScreen()
 
     return(
@@ -28,11 +25,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
                     <ToggleLikeMutation>
                         {
                             (toggleLike) => {
+                                console.log("toggleLike",toggleLike);
                                 const handleFavClick = () => {
-                                    !liked && toggleLike({variables: {
+                                    toggleLike({variables: {
                                         input: {id}
                                     }})
-                                    setLiked(!liked)
                                 }
                                 return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
                             }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import { ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
+import {  
+  ApolloClient, 
+  ApolloProvider, 
+  InMemoryCache,
+  ApolloLink,
+  HttpLink,
+  from
+} from '@apollo/client'
+import { onError } from '@apollo/client/link/error'
 import Context from './Context'
-
 import {App} from './App'
 
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = sessionStorage.getItem('token')
+
+  if(token) {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+  }
+
+  return forward(operation)
+})
+
+const errorMiddleware = onError(
+  ({networkError}) => {
+    if(networkError && networkError.result.code === 'invalid_token') {
+      sessionStorage.removeItem('token');
+      window.location = '/';
+    }
+  }
+)
+
 const client = new ApolloClient({
-  uri: 'https://petgram-api-daniel.now.sh/graphql',
   cache: new InMemoryCache(),
+  link: from([
+    errorMiddleware,
+    authMiddleware,
+    new HttpLink({
+      uri: 'https://petgram-api-daniel.now.sh/graphql'
+    })
+  ])
 })
 
 
